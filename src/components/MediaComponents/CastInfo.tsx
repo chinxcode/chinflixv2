69a9fad0f0f306c3fad18436b21bcf5e947ed469
sachin <sachinsharma11175@gmail.com>
2024-10-11 20:07:12 +0530

    Refactor CastInfo and SeasonEpisode components

--- src/components/MediaComponents/CastInfo.tsx
+++ src/components/MediaComponents/CastInfo.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import Image from "next/image";
 
 interface CastInfoProps {
     cast: {
@@ -9,23 +10,39 @@ interface CastInfoProps {
 }
 
 const CastInfo: React.FC<CastInfoProps> = ({ cast }) => {
+    const [showAll, setShowAll] = useState(false);
+    const initialDisplayCount = 4;
+
+    const toggleShowAll = () => {
+        setShowAll(!showAll);
+    };
+
+    const displayedCast = showAll ? cast : cast.slice(0, initialDisplayCount);
+
     return (
         <div className="flex flex-col">
             <div className="flex flex-col gap-2">
-                <div className="text-xl font-medium flex items-center w-full">
+                <div className="flex items-center w-full">
                     <span className="text-xl font-medium">Characters</span>
+                    {cast.length > initialDisplayCount && (
+                        <button onClick={toggleShowAll} className="text-xs ml-auto bubbly text-gray-400 hover:text-gray-200">
+                            {showAll ? "view less" : "view all"}
+                        </button>
+                    )}
                 </div>
                 <div className="flex flex-wrap gap-3">
-                    {cast.map((character, index) => (
+                    {displayedCast.map((character, index) => (
                         <div
                             key={index}
                             title={`${character.name} - ${character.actor}`}
                             className="w-full flex gap-1 h-20 rounded-xl overflow-hidden bg-white/5 hover:bg-white/[.03] smoothie shrink-0"
                         >
-                            <div className="h-full flex aspect-square rounded-xl overflow-hidden shrink-0">
-                                <img
+                            <div className="h-full flex aspect-square rounded-xl overflow-hidden shrink-0 relative">
+                                <Image
                                     src={character.image}
                                     alt={character.name}
+                                    layout="fill"
+                                    objectFit="cover"
                                     className="size-full object-cover object-center !select-none shrink-0"
                                 />
                             </div>
