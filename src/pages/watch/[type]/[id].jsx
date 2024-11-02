5b2c9c43b8882d8a23cb010427d985fe66d734bb
sachin <sachinsharma11175@gmail.com>
2024-11-02 17:38:27 +0530

    Add disclaimer to WatchPage about third-party media hosting

--- src/pages/watch/[type]/[id].jsx
+++ src/pages/watch/[type]/[id].jsx
@@ -111,6 +111,10 @@ const WatchPage = () => {
                                 }))}
                             />
                         </div>
+                        <p className="text-left py-4 text-sm text-gray-400 px-4">
+                            This site does not store any files on the server, we only linked to the media which is hosted on 3rd party
+                            services.
+                        </p>
                     </div>
                 </div>
 
