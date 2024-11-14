f3bea6b1ab833eadbf9bb3828b14007e2f558f97
sachin <sachinsharma11175@gmail.com>
2024-11-14 21:33:34 +0530

    Refactor VideoPlayer component and update streaming sources for improved video playback

--- src/components/MediaComponents/VideoPlayer.jsx
+++ src/components/MediaComponents/VideoPlayer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const VideoPlayer = ({ src }) => {
     return (
         <div className="w-full relative aspect-video bg-white/5 backdrop-blur-sm rounded-xl 2xl:rounded-2xl overflow-hidden shadow-md">
-            <iframe src={src} width="100%" height="100%" allowFullScreen className="size-full object-contain object-center" />
+            <iframe src={src} allowFullScreen className="size-full object-contain object-center" />
         </div>
     );
 };
--- src/lib/streamingSources.js
+++ src/lib/streamingSources.js
@@ -1,72 +1,77 @@
 export const streamingSources = {
-    "Server 1": {
-        flag: "https://flagcdn.com/w20/in.png",
-        movie: "https://kamitor316opk.com/play/{imdbId}",
-        tv: "https://kamitor316opk.com/play/{imdbId}?s={season}&e={episode}",
-    },
-    "Server 2": {
-        flag: "https://flagcdn.com/w20/us.png",
-        movie: "https://vidlink.pro/movie/{id}?icons=vid",
-        tv: "https://vidlink.pro/tv/{id}/{season}/{episode}?icons=vid&nextbutton=true",
-    },
-    "Server 3": {
-        flag: "https://flagcdn.com/w20/us.png",
-        movie: "https://vidsrc.pro/embed/movie/{id}?player=new",
-        tv: "https://vidsrc.pro/embed/tv/{id}/{season}/{episode}?player=new",
-    },
-    "Server 4": {
+    vidxyz: {
         flag: "https://flagcdn.com/w20/us.png",
         movie: "https://vidsrc.xyz/embed/movie?tmdb={id}",
         tv: "https://vidsrc.xyz/embed/tv?tmdb={id}&season={season}&episode={episode}",
     },
-    "Server 5": {
+    embedsu: {
+        flag: "https://flagcdn.com/w20/us.png",
+        movie: "https://embed.su/embed/movie/{id}",
+        tv: "https://embed.su/embed/tv/{id}/{season}/{episode}",
+    },
+    vidpro: {
+        flag: "https://flagcdn.com/w20/us.png",
+        movie: "https://vidlink.pro/movie/{id}?icons=vid",
+        tv: "https://vidlink.pro/tv/{id}/{season}/{episode}?icons=vid&nextbutton=true",
+    },
+    onexbet: {
+        flag: "https://flagcdn.com/w20/in.png",
+        movie: "https://kamitor316opk.com/play/{imdbId}",
+        tv: "https://kamitor316opk.com/play/{imdbId}?s={season}&e={episode}",
+    },
+    vidrip: {
         flag: "https://flagcdn.com/w20/gb.png",
         movie: "https://vidsrc.rip/embed/movie/{id}",
         tv: "https://vidsrc.rip/embed/tv/{id}/{season}/{episode}",
     },
-    "Server 6": {
+    vidcc: {
         flag: "https://flagcdn.com/w20/gb.png",
         movie: "https://vidsrc.cc/v2/embed/movie/{id}",
         tv: "https://vidsrc.cc/v2/embed/tv/{id}/{season}/{episode}",
     },
-    "Server 7": {
+    embedcc: {
         flag: "https://flagcdn.com/w20/us.png",
         movie: "https://www.2embed.cc/embed/{id}",
         tv: "https://www.2embed.cc/embedtv/{id}&s={season}&e={episode}",
     },
-    "Server 8": {
-        flag: "https://flagcdn.com/w20/gb.png",
-        movie: "https://embed-testing-v8.vercel.app/tests/rollerdice/{id}",
-        tv: "https://embed-testing-v8.vercel.app/tests/rollerdice/{id}-{season}-{episode}",
-    },
-    "Server 9": {
+    primewire: {
         flag: "https://flagcdn.com/w20/gb.png",
         movie: "https://www.primewire.tf/embed/movie?tmdb={id}",
         tv: "https://www.primewire.tf/embed/tv?tmdb={id}&season={season}&episode={episode}",
     },
-    "Server 10": {
+    multimov: {
         flag: "https://flagcdn.com/w20/us.png",
         movie: "https://multiembed.mov/?video_id={id}&tmdb=1",
         tv: "https://multiembed.mov/?video_id={id}&tmdb={season}&s=1&e={episode}",
     },
-    "Server 11": {
+    autocc: {
         flag: "https://flagcdn.com/w20/in.png",
-        movie: "https://player.autoembed.cc/embed/movie/{id}?server=2",
-        tv: "https://player.autoembed.cc/embed/tv/{id}/{season}/{episode}?server=2",
+        movie: "https://player.autoembed.cc/embed/movie/{id}",
+        tv: "https://player.autoembed.cc/embed/tv/{id}/{season}/{episode}",
     },
-    "Server 12": {
+    vidvip: {
         flag: "https://flagcdn.com/w20/in.png",
         movie: "https://vidsrc.vip/embed/movie/{id}&server=3",
         tv: "https://vidsrc.vip/embed/tv/{id}/{season}/{episode}&server=3",
     },
-    "Server 13": {
+    smashy: {
         flag: "https://flagcdn.com/w20/in.png",
         movie: "https://player.smashy.stream/movie/{id}",
         tv: "https://player.smashy.stream/tv/{id}?s={season}&e={episode}",
     },
-    "Server 14": {
+    vidbinge: {
         flag: "https://flagcdn.com/w20/us.png",
         movie: "https://www.vidbinge.com/media/tmdb-movie-{id}",
         tv: "https://www.vidbinge.com/media/tmdb-tv-{id}/{seasonId}/{episodeId}",
     },
 };
+
+/*
+
+    "Server 8": {
+        flag: "https://flagcdn.com/w20/gb.png",
+        movie: "https://embed-testing-v8.vercel.app/tests/rollerdice/{id}",
+        tv: "https://embed-testing-v8.vercel.app/tests/rollerdice/{id}-{season}-{episode}",
+    },
+
+*/
