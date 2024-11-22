962d43fb65d5a9b390efffacdb2a1ca2afe54dc3
sachin <sachinsharma11175@gmail.com>
2024-11-22 23:54:56 +0530

    Enhance CreateSection component layout with improved overflow handling and loading skeleton spacing

--- src/components/CreateSection.jsx
+++ src/components/CreateSection.jsx
@@ -97,14 +97,14 @@ const CreateSection = memo(({ type, endpoint, priority = false }) => {
                 </div>
             </div>
             <div className="relative">
-                <div ref={scrollContainerRef} className="flex gap-y-2 w-full overflow-x-auto scrollbar-hide">
+                <div ref={scrollContainerRef} className="flex gap-y-2 w-full h-full overflow-x-auto overflow-y-hidden scrollbar-hide">
                     {loading
                         ? Array(12)
                               .fill(0)
                               .map((_, i) => (
                                   <div
                                       key={i}
-                                      className="w-1/3 sm:w-1/4 md:w-1/5 lg:w-1/6 xl:w-[14.28%] 2xl:w-[12.5%] !aspect-[1.44/2] !shrink-0 sm:p-2"
+                                      className="w-1/3 sm:w-1/4 md:w-1/5 lg:w-1/6 xl:w-[14.28%] 2xl:w-[12.5%] !aspect-[1.44/2] !shrink-0 sm:p-2 gap-1"
                                   >
                                       <Skeleton className="w-full h-full rounded-xl" />
                                   </div>
