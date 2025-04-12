82e8fb6108acc2c6a549fd536899aa624bdcb847
Sachin <sachinsharma11175@gmail.com>
2025-04-13 02:48:59 +0530

    Remove unused AnimeComponents and update package versions

--- src/components/AnimeComponents.jsx
+++
@@ -1,4 +0,0 @@
-export { default as AnimeMediaActions } from "./AnimeComponents/AnimeMediaActions";
-export { default as AnimeMediaInfo } from "./AnimeComponents/AnimeMediaInfo";
-export { default as AnimeEpisodes } from "./AnimeComponents/AnimeEpisodes";
-export { default as AnimePlayer } from "./AnimeComponents/AnimePlayer";
