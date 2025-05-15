d316829ab469ae397a46f77f92cf98a01d49aaee
Sachin <sachinsharma11175@gmail.com>
2025-05-15 19:48:47 +0530

    feat: add Umami analytics script to _app component for tracking

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -6,6 +6,7 @@ import Head from "next/head";
 import { AnimatePresence } from "framer-motion";
 import { useRouter } from "next/router";
 import PageTransition from "@/components/PageTransition";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
     const router = useRouter();
@@ -16,6 +17,11 @@ export default function App({ Component, pageProps }) {
                 <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
                 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
             </Head>
+            <Script
+                src="https://cloud.umami.is/script.js"
+                data-website-id="7707589d-1468-4162-a3be-00adc5282cb1"
+                strategy="afterInteractive"
+            />
             <Layout>
                 <AnimatePresence mode="wait" initial={false}>
                     <PageTransition key={router.pathname}>
